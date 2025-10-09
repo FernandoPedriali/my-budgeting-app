eb7d81b2a097b963c72d485bb454f1a9c1def6b5
Fernando Rosa <fernandopedriali@gmail.com>
2025-10-09 17:15:40 -0300

    Avanço com estrutura, teste de botão transparente e com ícone svg

--- frontend_nicegui/shared/components/header.py
+++ frontend_nicegui/shared/components/header.py
@@ -3,10 +3,12 @@ from nicegui import ui
 
 def header():
     with ui.header().classes(
-        "bg-gray-800 text-white p-4 flex items-center justify-between"
+        "bg-black text-white p-4 flex items-center justify-between"
     ):
-        ui.label("My Application").classes("text-lg font-bold")
-        with ui.row().classes("gap-4"):
-            ui.button("Home").on("click", lambda: ui.notify("Home clicked"))
-            ui.button("About").on("click", lambda: ui.notify("About clicked"))
-            ui.button("Contact").on("click", lambda: ui.notify("Contact clicked"))
+        with ui.button(on_click=lambda: ui.notify("Check flat button clicked")
+                       ).props('flat').classes('text-black'):
+            ui.image("frontend_nicegui/shared/assets/svg/Flat_tick_icon.svg").classes('w-6 h-6')
+        ui.space()
+        ui.button("About").on("click", lambda: ui.notify("About clicked"))
+        ui.space()
+        ui.button("Contact").on("click", lambda: ui.notify("Contact clicked"))
